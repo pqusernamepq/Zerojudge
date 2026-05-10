d2adeea17f99d8cdb9d54255024c2275c3fed887
pqusernamepq <zhe99479@gmail.com>
2026-05-10 12:37:35 +0800

    Add files via upload

---
+++ a003.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int M, D;
+    cin >> M >> D;
+    if ((M*2+D)%3 == 0)
+    {
+        cout << "普通";
+    }
+    else if ((M*2+D)%3 == 1)
+    {
+        cout << "吉";
+    }
+    else
+    {
+        cout << "大吉";
+    }
+    return 0;
+}
